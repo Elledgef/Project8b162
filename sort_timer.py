4e76e4c30c32185a13417c4433cd9b087adc5503
Elledgef <elledgef@oregonstate.edu>
2022-11-16 15:48:28 -0800

    Final changes

--- sort_timer.py
+++ sort_timer.py
@@ -10,13 +10,13 @@ from functools import wraps
 
 
 def bubble_sort(num_list):
+
     for sort_num in range(len(num_list) - 1):
         for swap_num in range(len(num_list) - sort_num - 1):
             if num_list[swap_num] > num_list[swap_num + 1]:
                 temp = num_list[swap_num]
                 num_list[swap_num] = num_list[swap_num + 1]
                 num_list[swap_num + 1] = temp
-
 
 def sort_timer(func):
     @wraps(func)
